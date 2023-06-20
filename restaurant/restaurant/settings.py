22dbe5d0e4786e1f5ea56cb517212d25f428a087
PrabhuUdurg <egorpeshiy@gmail.com>
2023-06-20 13:00:05 +0200

    api, mysql

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +77,14 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'my_restaurant_database',
+        'USER':'admindjango',
+        'PASSWORD':'employee@123!',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- restaurant/web/views.py
+++ restaurant/web/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import views, generics
+
 
 # Create your views here.
+class MenuItemView(views.APIView):
+    
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    
